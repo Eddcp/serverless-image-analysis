3f9846c9d50a7e3b47e8c13dc282bce01c10ee8a
eddcp <eduardocspv@gmail.com>
2022-04-28 17:51:39 -0300

    Analysing image labels

--- handler.js
+++ handler.js
@@ -1,14 +1,34 @@
 'use strict';
 const aws = require('aws-sdk');
-const reko = new aws.Rekognition()
+const reko = new aws.Rekognition();
+const { promises : { readFile }} = require('fs');
 
 class Handler {
   constructor({ rekoSvc }) {
     this.rekoSvc = rekoSvc
   }
 
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc.detectLabels({
+      Image: {
+        Bytes:buffer
+      }
+    }).promise();
+
+    const workingItems = result.Labels
+      .filter(({ Confidence }) => Confidence > 80);
+
+    const animals = workingItems
+      .map(({ Name }) => Name)
+      .join(' and ');
+
+    console.log({ animals, workingItems});
+  }
+
   async main(event) {
     try {
+      const imgBuffer = await readFile('./images/pandora.jpeg');
+      await this.detectImageLabels(imgBuffer);
       return {
         statusCode: 200,
         body: 'Hello!'
@@ -23,9 +43,8 @@ class Handler {
   }
 }
 
-//factory
 const handler = new Handler({
   rekoSvc: reko
 });
 
-module.exports.main = handler.main.bind();
+module.exports.main = handler.main.bind(handler);
